7625ad6580474c52f8adaa8aa39f2eab65c83487
ha03441588567-sketch <ha03441588567@gmail.com>
2026-05-02 04:32:25 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title= "RAG Knowledge Assistant")
+
+app.add_middleware(CORSMiddle, allow_origins=["*"])
+
+@app.get("/")
+def read_root():
+  return{"message": "RAG Knowledge Assistant API"}
+
+@app.post("/upload")
+async def upload_file(file:UploadFile = File(...)):
+  return {"filename":file.filename, "status": "uploaded"}
+  
