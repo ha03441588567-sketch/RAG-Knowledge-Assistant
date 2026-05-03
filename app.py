6c66fa7ac4d30a519d19464f9a8521239464e8d1
ha03441588567-sketch <ha03441588567@gmail.com>
2026-05-03 05:34:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import streamlit as st
 import openai
 from PyPDF2 import PdfReader
 from langchain_text_splitters import CharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, OpenAI
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import RetrievalQA
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
+
 st.title("RAG Knowledge Assistant")
 
 uploaded_file = st.file_uploader("Upload PDF", type="pdf")
@@ -25,8 +26,7 @@ if uploaded_file is not None:
 
     question = st.text_input("Ask a question:")
     if question:
-        docs = db.similarity_search(question)
-        llm = OpenAI()
-        chain = load_qa_chain(llm, chain_type="stuff")
-        answer = chain.run(input_documents=docs, question=question)
+        llm = ChatOpenAI()
+        qa = RetrievalQA.from_chain_type(llm=llm, retriever=db.as_retriever())
+        answer = qa.run(question)
         st.write(answer)
